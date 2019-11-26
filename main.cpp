d5e4aa4c19f73de7ace3b04e0e871b7cc19c9dcc
hoenking <519142662@qq.com>
2019-11-26 22:16:28 +0800

    增加Sqlite3 测试类

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <sqlite3.h>
 
 #define ADD_CLASS_FIELD(type, name, getter, setter) \
 private:\
@@ -52,6 +53,61 @@ class CrossLine:public TestModel
     }
 };
 
+class SqliteDB:public TestModel
+{
+    // 暂时先不管 返回的数据
+    static int callback(void *NotUsed, int argc, char **argv, char **azColName)
+    {
+        int i = 0;
+        for(i = 0; i < argc; i++)
+        {
+            printf("%s = %s\n",azColName[i], argv[i]?argv[i]:"NULL");
+        }
+
+        printf("\n");
+        return 0;
+    }
+
+    void func() override
+    {
+        sqlite3* db;
+        std::string strName = "test.db";  //数据库句柄
+        char* zErrMsg;  //错误信息
+
+        //新创建或打开数据库
+        int res = sqlite3_open( strName.c_str(), &db);
+
+        //如果打开失败返回否则继续
+        if(res)
+        {
+            fprintf(stderr, "Can't open database:%s\n", sqlite3_errmsg(db));
+            return;
+        }
+        else
+        {
+            fprintf(stderr,"open database succeddfully\n");
+        }
+
+        //创建company 数据表的 sql语句
+        const char* sql = "create table company(ID INT PRIMARY KEY NOT NULL, NAME TEXT NOT NULL, AGE TEXT NOT NULL, ADDRESS CHAR(50))";
+
+        res = sqlite3_exec(db, sql, callback, nullptr, &zErrMsg); // 执行上面sql中的命令
+
+        //sql执行结果判断
+        if(SQLITE_OK != res)
+        {
+            fprintf(stderr, "SQL error: %s\n", zErrMsg);
+        }
+        else
+        {
+            fprintf(stdout, "create table successfully\n");
+        }
+
+        //关闭数据库
+        sqlite3_close(db);
+    }
+};
+
 class Factory
 {
     std::vector<TestModel*> ModelList;
@@ -80,6 +136,10 @@ int main()
 
     model = new CrossLine;
     model->setModelName("CrossLine");
+    factory.AddModel(model);
+
+    model = new SqliteDB;
+    model->setModelName("SqliteDB");
     factory.AddModel(model);
 
     for (auto l:factory.GetModelList()) {
